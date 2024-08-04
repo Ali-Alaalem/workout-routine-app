df910d6b5f2b5e829e32ba4db7d3805c4f9cb6bf
ali-alaalem <Meemo2002a@gmail.com>
2024-08-04 14:55:43 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ const App = () => {
       <h1>exercises in a workout routine List</h1>
       <ul>
         {items.map((item, index) => (
-          <li key={index} className={item.completed ? 'completed' : 'not-completed`'}>
+          <li key={index} className={item.completed ? 'completed' : 'not-completed'}>
             {item.name}
           </li>
         ))}
